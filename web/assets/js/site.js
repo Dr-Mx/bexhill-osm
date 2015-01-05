151a969174ca6c5a71c54e46b69886c8136092b1
Manuel Kaufmann <humitos@gmail.com>
2015-01-04 22:41:26 -0300

    OSM POIs v0.2
    Show only selected (amenity=clinic|pharmacy|fuel|bar) pois.

--- web/assets/js/site.js
+++ web/assets/js/site.js
@@ -10,6 +10,36 @@ L.tileLayer.provider('OpenStreetMap.Mapnik').addTo(map);
 // https://github.com/domoritz/leaflet-locatecontrol
 L.control.locate().addTo(map);
 
+var icons = {
+    clinic: {
+	name: 'Clínica',
+	amenity: 'clinic',
+	iconName: 'hospital-o',
+	markerColor: 'red'
+    },
+    pharmacy: {
+	name: 'Farmacia',
+	amenity: 'pharmacy',
+	iconName: 'plus-square',
+	markerColor: 'green'
+    },
+    fuel: {
+	name: 'Estación de Servicio',
+	amenity: 'fuel',
+	iconName: 'car',
+	markerColor: 'orange'
+    },
+    bar: {
+	name: 'Bar',
+	amenity: 'bar',
+	iconName: 'glass',
+	markerColor: 'blue'
+    }
+}
+
+var iconLayer = new L.LayerGroup();
+map.addLayer(iconLayer);
+
 // https://github.com/kartenkarsten/leaflet-layer-overpass
 function callback(data) {
     for(i=0; i < data.elements.length; i++) {
@@ -19,25 +49,69 @@ function callback(data) {
 	this.instance._ids[e.id] = true;
 
 	var pos = new L.LatLng(e.lat, e.lon);
-	var popup = this.instance._poiInfo(e.tags);
-	var icon  = L.AwesomeMarkers.icon({
-	    icon: 'hospital-o',
-	    markerColor: 'red',
+	console.debug(e.tags.amenity);
+	var icon = icons[e.tags.amenity];
+	var markerIcon  = L.AwesomeMarkers.icon({
+	    icon: icon.iconName,
+	    markerColor: icon.markerColor,
 	    prefix: 'fa'
 	});
-	var marker = L.marker(pos, {icon: icon})
+	var marker = L.marker(pos, {icon: markerIcon})
+	var markerPopup = '';
+	for(tag in e.tags) {
+	    markerPopup += Mustache.render(
+		'<strong>{{name}}:</strong> {{value}}<br>',
+		{name: tag, value: e.tags[tag]});
+	}
 
-	// TODO: add popup here
-
+	marker.bindPopup(markerPopup)
 	marker.addTo(this.instance);
     }
 }
 
+function build_overpass_query() {
+    amenity = [];
+    $('#settings input:checked').each(function(i, element) {
+	amenity.push(element.name);
+    });
 
-var opl = new L.OverPassLayer({
-  query: "node(BBOX)[amenity=clinic];out;",
-  callback: callback,
-  minzoom: 14,
-});
+    // TODO: improve this regex
+    amenity = amenity.join('|');
+    amenity = '^' + amenity.replace('|', '$|^') + '$';    
+}
 
-map.addLayer(opl);
+function setting_changed() {
+    // remove icons from current map
+    iconLayer.clearLayers();
+    build_overpass_query();
+    show_overpass_layer();
+}
+
+function show_settings() {
+    for(icon in icons) {
+	var checkbox = Mustache.render(
+	    '<input type="checkbox" checked="checked" name="{{icon}}" onclick="setting_changed()"> {{name}}<br>',
+	    {icon: icon, name: icons[icon].name}
+	)
+	$('#settings').append(checkbox);
+    }
+}
+show_settings();
+
+var amenity = [];
+build_overpass_query();
+
+function show_overpass_layer() {
+    var query = Mustache.render(
+	"node(BBOX)['amenity'~'{{amenity}}'];out;",
+	{amenity: amenity}
+    );
+    var opl = new L.OverPassLayer({
+	query: query,
+	callback: callback,
+	minzoom: 15,
+    });
+
+    iconLayer.addLayer(opl);
+}
+show_overpass_layer();
