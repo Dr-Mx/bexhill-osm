ab2c362589480094132be011bc55e7d3a322d1a5
Manuel Kaufmann <humitos@gmail.com>
2015-01-05 02:11:00 -0300

    Show more POIs

--- web/assets/js/site.js
+++ web/assets/js/site.js
@@ -11,30 +11,65 @@ L.tileLayer.provider('OpenStreetMap.Mapnik').addTo(map);
 L.control.locate().addTo(map);
 
 var icons = {
+    bank: {
+	name: 'Banco',
+	query: 'amenity=^bank$',
+	iconName: 'bank',
+	markerColor: 'cadetblue'
+    },
+    atm: {
+	name: 'Cajero Automático',
+	query: 'amenity=^atm$',
+	iconName: 'bank',
+	markerColor: 'cadetblue'
+    },
+
     clinic: {
 	name: 'Clínica',
-	amenity: 'clinic',
+	query: 'amenity=^clinic$',
 	iconName: 'hospital-o',
 	markerColor: 'red'
     },
     pharmacy: {
 	name: 'Farmacia',
-	amenity: 'pharmacy',
+	query: 'amenity=^pharmacy$',
 	iconName: 'plus-square',
 	markerColor: 'green'
     },
+
     fuel: {
 	name: 'Estación de Servicio',
-	amenity: 'fuel',
+	query: 'amenity=^fuel$',
 	iconName: 'car',
 	markerColor: 'orange'
     },
-    bar: {
-	name: 'Bar',
-	amenity: 'bar',
-	iconName: 'glass',
+
+    supermarket: {
+	name: 'Supermercado',
+	query: 'shop=^supermarket$',
+	iconName: 'calculator',
 	markerColor: 'blue'
+    },
+
+    'camp_site': {
+	name: 'Camping',
+	query: 'tourism=^camp_site$',
+	iconName: 'fire',
+	markerColor: 'green'
+    },
+    hotel: {
+	name: 'Hotel',
+	query: 'tourism=^hotel$',
+	iconName: 'building',
+	markerColor: 'darkred'
+    },
+    hostel: {
+	name: 'Hostel',
+	query: 'tourism=^hostel$',
+	iconName: 'building',
+	markerColor: 'darkred'
     }
+
 }
 
 var iconLayer = new L.LayerGroup();
@@ -49,15 +84,21 @@ function callback(data) {
 	this.instance._ids[e.id] = true;
 
 	var pos = new L.LatLng(e.lat, e.lon);
-	console.debug(e.tags.amenity);
-	var icon = icons[e.tags.amenity];
+
+	// TODO: improve this
+	var type;
+	if (e.tags.amenity) type = e.tags.amenity;
+	if (e.tags.tourism) type = e.tags.tourism;
+	if (e.tags.shop) type = e.tags.shop;
+	var icon = icons[type];
+
 	var markerIcon  = L.AwesomeMarkers.icon({
 	    icon: icon.iconName,
 	    markerColor: icon.markerColor,
 	    prefix: 'fa'
 	});
 	var marker = L.marker(pos, {icon: markerIcon})
-	var markerPopup = '';
+	var markerPopup = '<h3>Etiquetas</h3>';
 	for(tag in e.tags) {
 	    markerPopup += Mustache.render(
 		'<strong>{{name}}:</strong> {{value}}<br>',
@@ -70,46 +111,53 @@ function callback(data) {
 }
 
 function build_overpass_query() {
-    amenity = [];
+    keys = [];
+    values = [];
     $('#settings input:checked').each(function(i, element) {
-	amenity.push(element.name);
+	keys.push(element.dataset.key);
+	values.push(element.dataset.value);
     });
 
-    // TODO: improve this regex
-    amenity = amenity.join('|');
-    amenity = '^' + amenity.replace('|', '$|^') + '$';    
+    keys = keys.join('|');
+    values = values.join('|')
+
+    // TODO: when a building matches a POI it's not shown in the map
+    // because we need to filter by "way" instead of "node". In case,
+    // we filter by "way" we need to get the lat/lon of the centre
+    query = Mustache.render(
+	"node(BBOX)[~'{{keys}}'~'{{values}}'];out;",
+	{keys: keys, values: values}
+    );
 }
 
 function setting_changed() {
     // remove icons from current map
     iconLayer.clearLayers();
     build_overpass_query();
+    if(keys == '' || values == '') return
     show_overpass_layer();
 }
 
 function show_settings() {
     for(icon in icons) {
+	query = icons[icon].query.split('=');
 	var checkbox = Mustache.render(
-	    '<input type="checkbox" checked="checked" name="{{icon}}" onclick="setting_changed()"> {{name}}<br>',
-	    {icon: icon, name: icons[icon].name}
-	)
+	    '<input type="checkbox" checked="checked" data-key="{{key}}" data-value="{{value}}" onclick="setting_changed()"> {{name}}<br>',
+	    {icon: icon, key: query[0], value: query[1], name: icons[icon].name}
+	);
 	$('#settings').append(checkbox);
     }
 }
 show_settings();
 
-var amenity = [];
+var query = [];
 build_overpass_query();
 
 function show_overpass_layer() {
-    var query = Mustache.render(
-	"node(BBOX)['amenity'~'{{amenity}}'];out;",
-	{amenity: amenity}
-    );
     var opl = new L.OverPassLayer({
 	query: query,
 	callback: callback,
-	minzoom: 15,
+	minzoom: 14,
     });
 
     iconLayer.addLayer(opl);
