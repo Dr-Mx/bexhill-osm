6fa4e773e09f3e2d5b667eec836a7c33964bf07e
Manuel Kaufmann <humitos@gmail.com>
2015-01-16 03:48:42 -0300

    Icons on marker popups (#44)

--- web/assets/js/tags-parsers.js
+++ web/assets/js/tags-parsers.js
@@ -1,5 +1,5 @@
 var titleTmpl = '<h3>{{title}}</h3>';
-var tagTmpl = '<strong>{{tag}}:</strong> {{value}}<br>';
+var tagTmpl = '<span class="fa fa-{{iconName}}"></span> <strong>{{tag}}:</strong> {{value}}<br>';
 
 function develop_parser(element) {
     var tags = element.tags;
@@ -50,7 +50,7 @@ function address_parser(element) {
 	}
 	markerPopup += Mustache.render(
 	    tagTmpl,
-	    {tag: 'Dirección', value: value}
+	    {tag: 'Dirección', value: value, iconName: 'map-marker'}
 	);
     }
     return markerPopup;
@@ -70,20 +70,20 @@ function website_parser(element) {
 	);
 	markerPopup += Mustache.render(
 	    tagTmpl,
-	    {tag: 'Sitio Web', value: link}
+	    {tag: 'Sitio Web', value: link, iconName: 'globe'}
 	);
     }
     return markerPopup;
 }
 
-function generic_tag_parser(element, tag, tagName) {
+function generic_tag_parser(element, tag, tagName, iconName) {
     var tags = element.tags;
     var markerPopup = '';
 
     if (tags[tag]) {
 	markerPopup += Mustache.render(
 	    tagTmpl,
-	    {tag: tagName, value: tags[tag]}
+	    {tag: tagName, value: tags[tag], iconName: iconName}
 	);
     }
     return markerPopup;
@@ -94,14 +94,14 @@ function generic_yes_no_tag_parser(element, tag, tagName) {
     var markerPopup = '';
     var yes = false;
 
-    if (tags[tag] == 'yes') yes = 'Sí';
-    else if (tags[tag] == 'no') yes = 'No';
+    if (tags[tag] == 'yes') yes = '<span class="fa fa-check"></span>';
+    else if (tags[tag] == 'no') yes = '<span class="fa fa-remove"></span>';
 
     if (!yes) return ''
 
     markerPopup += Mustache.render(
 	tagTmpl,
-	{tag: tagName, value: yes}
+	{tag: tagName, value: yes, iconName: 'info-circle'}
     );
 
     return markerPopup;
@@ -143,7 +143,7 @@ function hotel_parser(element) {
 	element,
 	'Hotel',
 	[
-	    {callback: generic_tag_parser, arg1: 'stars', arg2: 'Estrellas'},
+	    {callback: generic_tag_parser, arg1: 'stars', arg2: 'Estrellas', iconName: 'star'},
 	]
     );
 }
@@ -172,18 +172,19 @@ function parse_tags(element, titlePopup, functions) {
     functions = [
 	{callback: generic_tag_parser, arg1: 'name', arg2: 'Nombre'},
 	{callback: address_parser},
-	{callback: generic_tag_parser, arg1: 'phone', arg2: 'Teléfono'},
-	{callback: generic_tag_parser, arg1: 'contact:phone', arg2: 'Teléfono'},
-	{callback: generic_tag_parser, arg1: 'contact:fax', arg2: 'Fax'},
-	{callback: generic_tag_parser, arg1: 'contact:email', arg2: 'Email'},
-	{callback: generic_tag_parser, arg1: 'email', arg2: 'Email'},
+	{callback: generic_tag_parser, arg1: 'phone', arg2: 'Teléfono', iconName: 'phone'},
+	{callback: generic_tag_parser, arg1: 'contact:phone', arg2: 'Teléfono', iconName: 'phone'},
+	{callback: generic_tag_parser, arg1: 'contact:fax', arg2: 'Fax', iconName: 'fax'},
+	{callback: generic_tag_parser, arg1: 'contact:email', arg2: 'Email', iconName: 'at'},
+	{callback: generic_tag_parser, arg1: 'email', arg2: 'Email', iconName: 'at'},
 	{callback: website_parser},
     ].concat(functions)
 
     for (var i = 0; i < functions.length; i++) {
 	var data = functions[i]
 	if (data.arg1 && data.arg2) {
-	    markerPopup += data.callback(element, data.arg1, data.arg2);
+	    var iconName = data.iconName ? data.iconName : 'info-circle';
+	    markerPopup += data.callback(element, data.arg1, data.arg2, iconName);
 	}
 	else {
 	    markerPopup += data.callback(element);
